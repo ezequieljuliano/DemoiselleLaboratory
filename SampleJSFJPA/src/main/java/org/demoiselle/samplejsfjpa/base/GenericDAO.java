b2b2fdef93de4b773448dc193936d89549636ff9
Ezequiel Juliano Müller <ezequieljuliano@gmail.com>
2015-11-12 08:21:44 -0200

    First Commit

---
+++ SampleJSFJPA/src/main/java/org/demoiselle/samplejsfjpa/base/GenericDAO.java
@@ -0,0 +1,9 @@
+package org.demoiselle.samplejsfjpa.base;
+
+import br.gov.frameworkdemoiselle.template.JPACrud;
+
+public abstract class GenericDAO<Domain, Key> extends JPACrud<Domain, Key> {
+
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ SampleJSFJPA/src/main/java/org/demoiselle/samplejsfjpa/view/BookmarkMB.java
@@ -0,0 +1,15 @@
+package org.demoiselle.samplejsfjpa.view;
+
+import br.gov.frameworkdemoiselle.annotation.NextView;
+import br.gov.frameworkdemoiselle.annotation.PreviousView;
+import br.gov.frameworkdemoiselle.stereotype.ViewController;
+import org.demoiselle.samplejsfjpa.base.GenericMB;
+import org.demoiselle.samplejsfjpa.business.BookmarkBC;
+import org.demoiselle.samplejsfjpa.domain.Bookmark;
+
+@ViewController
+@NextView("/bookmark-edit.xhtml")
+@PreviousView("/bookmark-list.xhtml")
+public class BookmarkMB extends GenericMB<Bookmark, Long, BookmarkBC> {
+   
+}
